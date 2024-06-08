146c08293b4accb2b153199d2bf114d2c6da46ca
chalwe mwansa <chalwemwansa23@gmail.com>
2024-06-08 04:30:48 +0200

    changed file 2

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -5,16 +5,22 @@ const calculateNumber = require('./1-calcul');
 const { expect } = require('chai');
 
 describe('Math functions', function () {
-  describe('#calculateNumber', function () {
+  describe('#SUM', function () {
     it('returns 6 for SUM 1.4 and 4.5', function () {
       expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
-    }); 
+    });
+  });
+  describe('#SUBTRACT', function () {
     it('returns -4 for SUBTRACT 1.4  and 4.5', function () {
       expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
-    }); 
+    });
+  });
+  describe('#DIVIDE', function () {
     it('returns 0.2 for DIVIDE 1.4 and 4.5', function () {
       expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
-    }); 
+    });
+  });
+  describe('#DIVIDE', function () {
     it('returns Error for DIVIDE 1.4 by 0', function () {
       expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
     });
